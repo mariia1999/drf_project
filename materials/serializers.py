9632a85af7b791da5290279ec344989b4ee81d0f
Maria Lomets <marusya.lomets@gmail.com>
2024-09-16 22:50:01 +0300

    serializers

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from materials.models import Course, Lesson
 
@@ -5,7 +7,18 @@ from materials.models import Course, Lesson
 class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = '__all__'
+
+
+class CourseDetailSerializer(ModelSerializer):
+    amount_of_lessons_in_course = SerializerMethodField()
+
+    def get_amount_of_lessons_in_course(self, lesson):
+        return Lesson.objects.filter(course=lesson.course).count()
+
+    class Meta:
+        model = Lesson
+        fields = ('lesson_name', 'lesson_description', 'course', 'amount_of_lessons_in_course', )
 
 
 class LessonSerializer(ModelSerializer):
