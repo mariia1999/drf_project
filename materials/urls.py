2a2d66bb7e02dc7669c6122e7141560a1f776ae5
Maria Lomets <marusya.lomets@gmail.com>
2024-09-19 11:27:44 +0300

    serializers

--- materials/urls.py
+++ materials/urls.py
@@ -11,10 +11,10 @@ router.register("", CourseViewSet)
 
 urlpatterns = [
     path("lesson/", LessonListApiView.as_view(), name="lesson_list"),
-    path("course/<int:pk>/", LessonRetrieveApiView.as_view(), name="course_retrieve"),
-    path("course/create/", LessonCreateApiView.as_view(), name="course_create"),
-    path("course/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="course_destroy"),
-    path("course/<int:pk>/update/", LessonUpdateApiView.as_view(), name="course_update"),
+    path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson_retrieve"),
+    path("lesson/create/", LessonCreateApiView.as_view(), name="lesson_create"),
+    path("lesson/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lesson_destroy"),
+    path("lesson/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lesson_update"),
 ]
 
 urlpatterns += router.urls
